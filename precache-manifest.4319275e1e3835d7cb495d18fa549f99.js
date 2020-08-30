7c990847f38dae484d76182894b360d6b41e3d93
mostafa ghazaly <ghazalymostafa07@gmail.com>
2020-08-31 00:31:27 +0200

    Updates

---
+++ precache-manifest.4319275e1e3835d7cb495d18fa549f99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777b27c64692d6c05b662a3968860357",
+    "url": "/Reminder-app/index.html"
+  },
+  {
+    "revision": "5c03c6d04dbb892d96d0",
+    "url": "/Reminder-app/static/css/2.0cee8fd6.chunk.css"
+  },
+  {
+    "revision": "22b91980d55444f49d70",
+    "url": "/Reminder-app/static/css/main.b24bdad2.chunk.css"
+  },
+  {
+    "revision": "5c03c6d04dbb892d96d0",
+    "url": "/Reminder-app/static/js/2.5963a64d.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/Reminder-app/static/js/2.5963a64d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b91980d55444f49d70",
+    "url": "/Reminder-app/static/js/main.cd0db285.chunk.js"
+  },
+  {
+    "revision": "742ddce8e02ac6d1078b",
+    "url": "/Reminder-app/static/js/runtime-main.18739353.js"
+  }
+]);
